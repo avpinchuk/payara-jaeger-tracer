d965694086b47a14763dcbcba6055870f29f2f06
alexa <alexander.v.pinchuk@gmail.com>
2020-08-31 08:30:08 +0300

    Add jaeger-thrift tests

---
+++ jaeger-thrift/src/test/java/io/jaegertracing/thrift/internal/senders/UdpSenderTest.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright (c) 2016, Uber Technologies, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.jaegertracing.thrift.internal.senders;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import io.jaegertracing.internal.JaegerSpan;
+import io.jaegertracing.internal.JaegerTracer;
+import io.jaegertracing.internal.exceptions.SenderException;
+import io.jaegertracing.internal.metrics.InMemoryMetricsFactory;
+import io.jaegertracing.internal.reporters.InMemoryReporter;
+import io.jaegertracing.spi.Reporter;
+import io.jaegertracing.internal.samplers.ConstSampler;
+import io.jaegertracing.thrift.internal.reporters.protocols.JaegerThriftSpanConverter;
+import io.jaegertracing.thrift.internal.reporters.protocols.TestTServer;
+import io.jaegertracing.thriftjava.Batch;
+import io.jaegertracing.thriftjava.Process;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+import org.awaitility.Awaitility;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UdpSenderTest {
+    static final String SERVICE_NAME = "test-sender";
+    final String destHost = "localhost";
+    int destPort;
+    final int maxPacketSize = 1000;
+
+    JaegerTracer tracer;
+    JaegerTracer tracer128;
+    Reporter reporter;
+    UdpSender sender;
+    TestTServer server;
+
+    private TestTServer startServer() throws Exception {
+        TestTServer server = new TestTServer(0);
+        destPort = server.getPort();
+
+        Thread t = new Thread(server);
+        t.start();
+
+        // wait up to 5 seconds to get this thread started
+        Awaitility.await()
+                  .with()
+                  .pollInterval(1, TimeUnit.MILLISECONDS)
+                  .atMost(5, TimeUnit.SECONDS)
+                  .until(t::isAlive);
+        return server;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        server = startServer();
+        reporter = new InMemoryReporter();
+        final JaegerTracer.Builder tracerBuilder = new JaegerTracer.Builder(SERVICE_NAME)
+                .withReporter(reporter)
+                .withSampler(new ConstSampler(true))
+                .withMetricsFactory(new InMemoryMetricsFactory())
+                .withTag("foo", "bar");
+        tracer = tracerBuilder.build();
+        tracer128 = tracerBuilder.withTraceId128Bit().build();
+        sender = new UdpSender(destHost, destPort, maxPacketSize);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.close();
+        sender.close();
+        reporter.close();
+    }
+
+    /**
+     * create a mock span with target size
+     */
+    private JaegerSpan buildSpanWithSize(int targetSpanSize) throws Exception {
+        JaegerSpan jaegerSpan = tracer.buildSpan("raza").start();
+        jaegerSpan.setTag("mock", "");
+
+        // contains mock span tag with empty string value, which is later used to match target span size
+        io.jaegertracing.thriftjava.Span emptySpan = JaegerThriftSpanConverter.convertSpan(jaegerSpan);
+
+        int emptySpanSize = sender.getSize(emptySpan);
+        int freePacketSizeLeft = targetSpanSize - emptySpanSize;
+
+        // each "0" takes 1 byte in UTF-8
+        String mockStr = String.join("", Collections.nCopies(freePacketSizeLeft, "0"));
+        jaegerSpan.setTag("mock", mockStr);
+
+        // subtract some chars since the actual encoding takes a few extra bytes
+        while (targetSpanSize != sender.getSize(JaegerThriftSpanConverter.convertSpan(jaegerSpan))) {
+            mockStr = mockStr.substring(1);
+            jaegerSpan.setTag("mock", mockStr);
+        }
+
+        return jaegerSpan;
+    }
+
+    @Test
+    public void testAppend() throws Exception {
+        // find size of the initial span
+        JaegerSpan jaegerSpan = tracer.buildSpan("raza").start();
+        io.jaegertracing.thriftjava.Span span = JaegerThriftSpanConverter.convertSpan(jaegerSpan);
+
+        Process process = new Process(tracer.getServiceName())
+                .setTags(JaegerThriftSpanConverter.buildTags(tracer.tags()));
+
+        int processSize = sender.getSize(process);
+        int spanSize = sender.getSize(span);
+
+        // create a sender that a multiple of the span size (accounting for span overhead)
+        // this allows us to test the boundary conditions of writing spans.
+        int expectedNumSpans = 11;
+        int maxPacketSize = (spanSize * expectedNumSpans) + UdpSender.EMIT_BATCH_OVERHEAD + processSize;
+        int maxPacketSizeLeft = maxPacketSize - UdpSender.EMIT_BATCH_OVERHEAD - processSize;
+        // add enough spans to be under buffer limit
+        sender = new UdpSender(destHost, destPort, maxPacketSize);
+        while (spanSize < maxPacketSizeLeft) {
+            sender.append(jaegerSpan);
+            maxPacketSizeLeft -= spanSize;
+        }
+
+        // add a span that overflows the limit to hit the last branch
+        int result = sender.append(jaegerSpan);
+
+        assertEquals(expectedNumSpans, result);
+    }
+
+    @Test
+    public void testAppendMaxSize() throws Exception {
+        Process process = new Process(tracer.getServiceName())
+                .setTags(JaegerThriftSpanConverter.buildTags(tracer.tags()));
+
+        int processSize = sender.getSize(process);
+
+        JaegerSpan jaegerSpan = buildSpanWithSize(maxPacketSize - UdpSender.EMIT_BATCH_OVERHEAD - processSize);
+
+        int result = sender.append(jaegerSpan);
+
+        assertEquals(1, result);
+
+        // test if the buffer is reinitialized correctly
+        result = sender.append(jaegerSpan);
+
+        assertEquals(1, result);
+    }
+
+    @Test(expected = SenderException.class)
+    public void testAppendSpanTooLarge() throws Exception {
+        Process process = new Process(tracer.getServiceName())
+                .setTags(JaegerThriftSpanConverter.buildTags(tracer.tags()));
+        int processSize = sender.getSize(process);
+
+        JaegerSpan jaegerSpan = buildSpanWithSize(maxPacketSize - UdpSender.EMIT_BATCH_OVERHEAD - processSize + 1);
+
+        try {
+            sender.append(jaegerSpan);
+        } catch (SenderException e) {
+            assertEquals(e.getDroppedSpanCount(), 1);
+            throw e;
+        }
+    }
+
+    @Test
+    public void testFlushSendsSpan() throws Exception {
+        int timeout = 50; // in milliseconds
+        int expectedNumSpans = 1;
+        JaegerSpan expectedSpan = tracer.buildSpan("raza").start();
+        int appendNum = sender.append(expectedSpan);
+        int flushNum = sender.flush();
+        assertEquals(appendNum, 0);
+        assertEquals(flushNum, 1);
+
+        Batch batch = server.getBatch(expectedNumSpans, timeout);
+        assertEquals(expectedNumSpans, batch.getSpans().size());
+
+        io.jaegertracing.thriftjava.Span actualSpan = batch.getSpans().get(0);
+        assertEquals(expectedSpan.context().getTraceIdLow(), actualSpan.getTraceIdLow());
+        assertEquals(0, actualSpan.getTraceIdHigh());
+        assertEquals(expectedSpan.context().getSpanId(), actualSpan.getSpanId());
+        assertEquals(0, actualSpan.getParentSpanId());
+        assertTrue(actualSpan.references.isEmpty());
+        assertEquals(expectedSpan.getOperationName(), actualSpan.getOperationName());
+        assertEquals(4, batch.getProcess().getTags().size());
+        assertEquals("hostname", batch.getProcess().getTags().get(0).getKey());
+        assertEquals("jaeger.version", batch.getProcess().getTags().get(1).getKey());
+        assertEquals("bar", batch.getProcess().getTags().get(2).getVStr());
+        assertEquals("ip", batch.getProcess().getTags().get(3).getKey());
+    }
+
+    @Test
+    public void testFlushSendsSpanWith128BitTraceId() throws Exception {
+        int timeout = 50; // in milliseconds
+        int expectedNumSpans = 1;
+        JaegerSpan expectedSpan = tracer128.buildSpan("raza").start();
+        assertEquals(0, sender.append(expectedSpan));
+        assertEquals(1, sender.flush());
+
+        Batch batch = server.getBatch(expectedNumSpans, timeout);
+        assertEquals(expectedNumSpans, batch.getSpans().size());
+
+        io.jaegertracing.thriftjava.Span actualSpan = batch.getSpans().get(0);
+        assertEquals(expectedSpan.context().getTraceIdLow(), actualSpan.getTraceIdLow());
+        assertEquals(expectedSpan.context().getTraceIdHigh(), actualSpan.getTraceIdHigh());
+    }
+
+    @Test(expected = SenderException.class)
+    public void senderFail() throws Exception {
+        sender.send(null, Collections.emptyList());
+    }
+}
